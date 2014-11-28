8a8746629bc5a87f324790e0af7895ed956ba9f5
Andrei Freeman <andrei@conbuilders.com>
2014-11-28 15:43:35 -0500

    Preliminary UI

--- LogParse/ViewController.m
+++ LogParse/ViewController.m
@@ -8,6 +8,14 @@
 
 #import "ViewController.h"
 
+@interface ViewController ()
+
+@property (weak) IBOutlet NSTextField *countProcess;
+@property (weak) IBOutlet NSTextField *countError;
+@property (weak) IBOutlet NSPathControl *filePath;
+
+@end
+
 @implementation ViewController
 
 - (void)viewDidLoad {
