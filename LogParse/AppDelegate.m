da28fb450f5787afadf595bbd8629fa1a37f2b79
Andrei Freeman <andrei@conbuilders.com>
2014-11-28 15:38:11 -0500

    Remove Commented code

--- LogParse/AppDelegate.m
+++ LogParse/AppDelegate.m
@@ -43,12 +43,6 @@
                     encoding:NSUTF8StringEncoding
                        error:&anError];
     NSLog(@"\n\nSaved");
-
-    //    NSString *logRawLine = @"www.lordandrei.com 173.75.40.109 - - 18/Nov/2014 22:52:56 -0500 \"GET /blog/wp-admin/load-scripts.php?c=1&load%5B%5D=jquery-core,jquery-migrate,utils,jquery-ui-core,jquery-ui-widget,jquery-ui-mouse,jquery-ui-resizable,jquery-ui-draggable,jquery-&load%5B%5D=ui-button,jquery-ui-position,jquery-ui-dialog&ver=3.8.1 HTTP/1.1\" 200 61374 \"http://www.lordandrei.com/blog/wp-admin/edit.php\" \"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10) AppleWebKit/600.1.25 (KHTML, like Gecko) Version/8.0 Safari/600.1.25\" 1645 61837 /Library/Server/Web/Data/Sites/lordandrei/blog/wp-admin/load-scripts.php";
-    
-//    LogLine* aLogLine = [LogLine LogLineFromParsableLine:logRawLine];
-//    NSLog(@"aLogLine: %@", aLogLine);
-//    [aLogLine printInstanceData];
     
 }
 
